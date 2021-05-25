307415009bbcdbef1ed20087c76cf38f7ee553ed
Lisenkov Denis <denis.lisenkov@mail.ru>
2021-05-25 16:07:06 +0300

    Add js-script, hovers, regular expression, template engine

---
+++ Project/script/form.js
@@ -0,0 +1,25 @@
+var modal = document.getElementsByClassName("modal")[0];
+
+var btn = document.getElementsByClassName("help")[0];
+
+var span = document.getElementsByClassName("close")[0];
+
+var modal_content = $('.modal_content');
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  modal_content.animate({'height': 290, 'width': 450}, 1000);
+}
+
+span.onclick = function() {
+  modal_content.animate({'height': 0, 'width': 0}, 1000);
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal_content.animate({'height': 0, 'width': 0}, 1000);
+    modal.style.display = "none";
+  }
+}
+
---
+++ Project/script/regular.js
@@ -0,0 +1,9 @@
+function check_pattern(email) {
+    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(pattern)) {
+        return true;
+    }
+    return false;
+}
+
+console.log(check_pattern('asdsad@2333.com'))
