c96890820940e17099471a5315c6f10029f41274
Denis Lisenkov <lisenkov.d.y@gmail.com>
2023-03-11 18:13:51 +0300

    Formatted code

--- fashion-shop/script/form.js
+++ fashion-shop/script/form.js
@@ -4,22 +4,21 @@ var btn = document.getElementsByClassName("help")[0];
 
 var span = document.getElementsByClassName("close")[0];
 
-var modal_content = $('.modal_content');
+var modal_content = $(".modal_content");
 
-btn.onclick = function() {
-  modal.style.display = "block";
-  modal_content.animate({'height': 290, 'width': 450}, 1000);
-}
+btn.onclick = function () {
+    modal.style.display = "block";
+    modal_content.animate({ height: 290, width: 450 }, 1000);
+};
 
-span.onclick = function() {
-  modal_content.animate({'height': 0, 'width': 0}, 1000);
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal_content.animate({'height': 0, 'width': 0}, 1000);
+span.onclick = function () {
+    modal_content.animate({ height: 0, width: 0 }, 1000);
     modal.style.display = "none";
-  }
-}
+};
 
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal_content.animate({ height: 0, width: 0 }, 1000);
+        modal.style.display = "none";
+    }
+};
--- fashion-shop/script/regular.js
+++ fashion-shop/script/regular.js
@@ -1,9 +1,10 @@
 function check_pattern(email) {
-    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(email.match(pattern)) {
+    const pattern =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(pattern)) {
         return true;
     }
     return false;
 }
 
-console.log(check_pattern('asdsad@2333.com'))
+console.log(check_pattern("asdsad@2333.com"));
